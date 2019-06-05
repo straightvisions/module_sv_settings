b47982b57b4a0a0421a5c42920dc9889b474cb88
Adrian Chudzynski <adrian-chudzynsk@straightvisions.com>
2019-06-05 19:30:11 +0200

    init

--- lib/backend/tpl/tools.php
+++ lib/backend/tpl/tools.php
@@ -1,11 +1,41 @@
 <?php
-	/**
-	 * tools.php is a sample class for demonstrating PHPDoc
-	 *
-	 * @package     tools.php
-	 * @author      straightvisions
-	 * @version     1.0
-	 * @access      public
-	 * @since       7.3
-	 * @see         https://straightvisions.com/
-	 */
+	$module = $this->get_root()->sv_settings;
+	
+	if ( isset( $_POST[ $module->get_prefix( 'import' ) ] ) ) {
+		$file = $_FILES[ $module->get_prefix( 'import_file' ) ];
+		$data = json_decode( file_get_contents( $file['tmp_name'] ), true );
+		
+		foreach ( $data as $prefix => $settings ) {
+			if ( ! empty( $settings ) ) {
+				foreach ( $settings as $setting => $value ) {
+					if ( ! empty( $value ) ) {
+						$option = $prefix . '_settings_' . $setting;
+						error_log($option);
+						
+						update_option( $option, $value, true );
+					}
+				}
+			}
+		}
+	}
+?>
+
+<h3 class="divider"><?php _e('Export', $module->get_module_name() ); ?></h3>
+<p><?php _e( 'Click on the button, to export your settings.', $this->get_root()->get_prefix() ); ?></p>
+<a href="https://lab.straightvisions.com/wp-admin/admin-ajax.php?action=sv_100_sv_settings_export">
+	<?php _e( 'Export Settings', $module->get_module_name() ); ?>
+</a>
+<div>
+	<form method="post" action="" enctype="multipart/form-data">
+		<input id="<?php echo $module->get_prefix( 'import_file' ); ?>"
+			   name="<?php echo $module->get_prefix( 'import_file' ); ?>"
+			   type="file"
+			   accept=".json"
+			   placeholder="<?php _e( 'Import Settings', $module->get_module_name() ); ?>"
+		>
+
+		<button name="<?php echo $module->get_prefix( 'import' ); ?>">
+			<?php _e( 'Import Settings', $module->get_module_name() ); ?>
+		</button>
+	</form>
+</div>
--- sv_settings.php
+++ sv_settings.php
@@ -11,7 +11,7 @@ namespace sv_100;
  * @license			See license.txt or https://straightvisions.com
  */
 
-class sv_excerpt extends init {
+class sv_settings extends init {
 	public function __construct() {
 	
 	}
@@ -21,43 +21,49 @@ class sv_excerpt extends init {
 		load_theme_textdomain( $this->get_module_name(), $this->get_path( 'languages' ) );
 
 		// Module Info
-		$this->set_module_title( 'SV Excerpt' );
-		$this->set_module_desc( __( 'This module gives the ability to define how excerpts will be displayed.', $this->get_module_name() ) );
+		$this->set_module_title( 'SV Settings' );
+		$this->set_module_desc( __( 'Import and export your settings.', $this->get_module_name() ) );
 
 		// Section Info
-		$this->set_section_title( 'Excerpt' );
-		$this->set_section_desc( __( 'Adjust Settings', $this->get_module_name() ) );
-		$this->set_section_type( 'settings' );
-		$this->get_root()->add_section( $this );
+		$this->set_section_title( __( 'Settings Import/Export', $this->get_module_name() ) );
+		$this->set_section_desc( __( 'Import and export your settings', $this->get_module_name() ) );
+		$this->set_section_type( 'tools' );
+		$this->get_root()->add_section( $this )
+			 ->set_section_template_path( $this->get_path( 'lib/backend/tpl/tools.php' ) );
 
 		// Loads Settings
 		$this->load_settings();
-
+		
 		// Action Hooks
-		add_filter( 'excerpt_length', array( $this, 'excerpt_length' ) );
-		add_filter( 'excerpt_more', array( $this, 'excerpt_more' ) );
+		add_action( 'wp_ajax_' . $this->get_prefix( 'export' ) , array( $this, 'settings_export' ) );
 	}
 
-	public function load_settings() {
-		$this->s['length'] = static::$settings->create( $this )
-			->set_ID( 'length' )
-			->set_title( __( 'Excerpt length', $this->get_module_name() ) )
-			->set_description( __( 'Maximum number of words allowed in displaying excerpts.', $this->get_module_name() ) )
-			->set_placeholder( '80' )
-			->load_type( 'number' );
+	protected function load_settings(): sv_settings {
+		$this->s['export'] = static::$settings->create( $this )
+			->set_ID( 'export' )
+			->set_title( __( 'Export', $this->get_module_name() ) )
+			->set_description( __( 'Check the checkbox and click on "Save settings", to export your module settings.', $this->get_module_name() ) )
+			->load_type( 'checkbox' );
 
-		$this->s['more'] = static::$settings->create( $this )
-			->set_ID( 'more' )
-			->set_title( __( 'Text to show at the end of the excerpt', $this->get_module_name() ) )
-			->set_placeholder( '...' )
-			->load_type( 'text' );
+		$this->s['import'] = static::$settings->create( $this )
+			->set_ID( 'import' )
+			->set_title( __( 'Import your module settings', $this->get_module_name() ) )
+			->load_type( 'upload' );
+		
+		$this->s['import']->run_type()->set_allowed_filetypes( array( '.json' ) );
+		
+		return $this;
 	}
-
-	public function excerpt_length( $length ) {
-		return $this->s['length']->run_type()->get_data() ? $this->s['length']->run_type()->get_data() : 80;
-	}
-
-	public function excerpt_more() {
-		return $this->s['more']->run_type()->get_data() ? $this->s['more']->run_type()->get_data() : '...';
+	
+	public function settings_export() {
+		$file['name']	= $this->get_prefix( 'export_' . current_time( 'YmdHis' ) . '.json' );
+		$file['data']	= wp_json_encode( $this->get_modules_settings() );
+		
+		header('Content-Type: application/json');
+		header('Content-Disposition: attachment;filename="' . $file['name'] . '"');
+		
+		echo $file['data'];
+		
+		wp_die();
 	}
 }
