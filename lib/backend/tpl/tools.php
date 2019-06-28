92dca30f6a9491ee7efdd85bb1c16bdfd6aa1342
Adrian Chudzynski <adrian-chudzynsk@straightvisions.com>
2019-06-28 18:01:20 +0200

    Updated settings methods

--- lib/backend/tpl/tools.php
+++ lib/backend/tpl/tools.php
@@ -1,37 +1,38 @@
 <?php
-	$sv_settings = $this->get_root()->sv_settings;
-	
-	if ( isset( $_POST[ $sv_settings->get_prefix( 'import' ) ] ) ) {
-		$file = $_FILES[ $sv_settings->get_prefix( 'import_file' ) ];
-		$data = file_get_contents( $file['tmp_name'] );
-		
-		$sv_settings->settings_import( $data );
+	if ( isset( $_POST[ $this->get_module( 'sv_settings' )->get_prefix( 'import' ) ] ) ) {
+		$file = $_FILES[ $this->get_module( 'sv_settings' )->get_prefix( 'import_file' ) ];
+
+		if ( $file['size'] > 0 ) {
+			$data = file_get_contents( $file['tmp_name'] );
+			
+			$this->get_module( 'sv_settings' )->settings_import( $data );
+		}
 	}
 ?>
 
-<div class="<?php echo $sv_settings->get_prefix(); ?>">
+<div class="<?php echo $this->get_module( 'sv_settings' )->get_prefix(); ?>">
 	<form method="post" action="" enctype="multipart/form-data">
-		<div class="sv_setting <?php echo $sv_settings->get_prefix( 'import' ); ?>">
+		<div class="sv_setting <?php echo $this->get_module( 'sv_settings' )->get_prefix( 'import' ); ?>">
 			<h4><?php _e( 'Import', 'sv100' ); ?></h4>
 			<div class="description"><?php _e( 'Select your settings file and click on "Import Settings".<br>Allowed filetypes: .json', 'sv100' ); ?></div>
-			<label for="<?php echo $sv_settings->get_prefix( 'import_file' ); ?>">
+			<label for="<?php echo $this->get_module( 'sv_settings' )->get_prefix( 'import_file' ); ?>">
 				<input data-sv_type="sv_form_field"
 					   class="sv_file"
-					   name="<?php echo $sv_settings->get_prefix( 'import_file' ); ?>"
+					   name="<?php echo $this->get_module( 'sv_settings' )->get_prefix( 'import_file' ); ?>"
 					   type="file"
 					   accept=".json"
 					   placeholder="<?php _e( 'Settings file', 'sv100' ); ?>"
 				>
-				<button name="<?php echo $sv_settings->get_prefix( 'import' ); ?>" class="button">
+				<button name="<?php echo $this->get_module( 'sv_settings' )->get_prefix( 'import' ); ?>" class="button">
 					<?php _e( 'Import Settings', 'sv100' ); ?>
 				</button>
 			</label>
 		</div>
 	</form>
 
-	<div class="sv_setting <?php echo $sv_settings->get_prefix( 'export' ); ?>">
+	<div class="sv_setting <?php echo $this->get_module( 'sv_settings' )->get_prefix( 'export' ); ?>">
 		<h4><?php _e( 'Export', 'sv100' ); ?></h4>
-		<label for="<?php echo $sv_settings->get_prefix( 'export' ); ?>">
+		<label for="<?php echo $this->get_module( 'sv_settings' )->get_prefix( 'export' ); ?>">
 			<a href="https://lab.straightvisions.com/wp-admin/admin-ajax.php?action=sv100_sv_settings_export"
 			   class="button">
 				<?php _e( 'Export Settings', 'sv100' ); ?>
