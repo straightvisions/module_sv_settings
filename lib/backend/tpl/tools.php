d6a1634956f419d85ea536248d0c5c3c6079924c
Adrian Chudzynski <adrian-chudzynsk@straightvisions.com>
2019-07-18 21:00:20 +0200

    Updating settings logic

--- lib/backend/tpl/tools.php
+++ lib/backend/tpl/tools.php
@@ -1,20 +1,50 @@
 <?php
-	if ( isset( $_POST[ $this->get_module( 'sv_settings' )->get_prefix( 'import' ) ] ) ) {
+	if (
+		isset( $_GET[ $this->get_module( 'sv_settings' )->get_prefix( 'import' ) ] )
+		&& wp_verify_nonce( $_GET[ $this->get_module( 'sv_settings' )->get_prefix( 'import' ) ], $this->get_module( 'sv_settings' )->get_prefix( 'import' ) )
+	) {
 		$file = $_FILES[ $this->get_module( 'sv_settings' )->get_prefix( 'import_file' ) ];
-
+		
 		if ( $file['size'] > 0 ) {
 			$data = file_get_contents( $file['tmp_name'] );
 			
 			$this->get_module( 'sv_settings' )->settings_import( $data );
 		}
 	}
+	
+	$import_url = wp_nonce_url(
+		admin_url() . 'themes.php?page=sv100#section_sv100_sv_settings',
+		$this->get_module( 'sv_settings' )->get_prefix( 'import' ),
+		$this->get_module( 'sv_settings' )->get_prefix( 'import' ) );
+	$export_url = admin_url( 'admin-ajax.php' ) . '?action=' . $this->get_module( 'sv_settings' )->get_prefix( 'export' )
+				  . '&' . $this->get_module( 'sv_settings' )->get_prefix( 'export_nonce' ) . '=' . wp_create_nonce( $this->get_module( 'sv_settings' )->get_prefix( 'export' ) );
+	
+	$import_modal = array(
+		'title' => __( 'Import Settings', 'sv100' ),
+		'desc'	=> __( 'All your settings will be removed and replaced with the new settings.', 'sv100' ) . '<br>' .
+					 __( 'Do you want to proceed?', 'sv100' ),
+		'type'	=> 'confirm'
+	);
+	
+	$reset_modal = array(
+		'title' => __( 'Reset Settings', 'sv100' ),
+		'desc'	=> __( 'All your settings will be removed and replaced with the default settings.', 'sv100' ) . '<br>' .
+					 __( 'Do you want to proceed?', 'sv100' ),
+		'type'	=> 'confirm'
+	);
 ?>
 
 <div class="<?php echo $this->get_module( 'sv_settings' )->get_prefix(); ?>">
-	<form method="post" action="" enctype="multipart/form-data">
+	<form method="post" action="<?php echo $import_url; ?>" enctype="multipart/form-data" id="<?php echo $this->get_module( 'sv_settings' )->get_prefix( 'import' ); ?>">
+		<?php wp_nonce_field( $this->get_module( 'sv_settings' )->get_prefix( 'import' ), $this->get_module( 'sv_settings' )->get_prefix( 'import' ) ); ?>
 		<div class="sv_setting <?php echo $this->get_module( 'sv_settings' )->get_prefix( 'import' ); ?>">
 			<h4><?php _e( 'Import', 'sv100' ); ?></h4>
-			<div class="description"><?php _e( 'Select your settings file and click on "Import Settings".<br>Allowed filetypes: .json', 'sv100' ); ?></div>
+			<div class="description">
+				<?php
+					echo __( 'Select your settings file and click on "Import Settings".', 'sv100' ) . '<br>' .
+						 __( 'Allowed filetypes: .json', 'sv100' );
+				?>
+			</div>
 			<label for="<?php echo $this->get_module( 'sv_settings' )->get_prefix( 'import_file' ); ?>">
 				<input data-sv_type="sv_form_field"
 					   class="sv_file"
@@ -23,7 +53,10 @@
 					   accept=".json"
 					   placeholder="<?php _e( 'Settings file', 'sv100' ); ?>"
 				>
-				<button name="<?php echo $this->get_module( 'sv_settings' )->get_prefix( 'import' ); ?>" class="button">
+				<button class="button"
+						type="button"
+						data-sv_admin_modal='[{"title":"<?php echo $import_modal['title']; ?>","desc":"<?php echo $import_modal['desc']; ?>","type":"<?php echo $import_modal['type']; ?>","args":{"form":"<?php echo $this->get_module( 'sv_settings' )->get_prefix( 'import' ); ?>"}}]'
+				>
 					<?php _e( 'Import Settings', 'sv100' ); ?>
 				</button>
 			</label>
@@ -33,10 +66,27 @@
 	<div class="sv_setting <?php echo $this->get_module( 'sv_settings' )->get_prefix( 'export' ); ?>">
 		<h4><?php _e( 'Export', 'sv100' ); ?></h4>
 		<label for="<?php echo $this->get_module( 'sv_settings' )->get_prefix( 'export' ); ?>">
-			<a href="<?php echo admin_url( 'admin-ajax.php' ); ?>?action=sv100_sv_settings_export"
-			   class="button">
+			<button
+					class="button"
+					data-sv_admin_ajax='[{"action":"<?php echo $this->get_module( 'sv_settings' )->get_prefix( 'export' ); ?>"}]'
+			>
 				<?php _e( 'Export Settings', 'sv100' ); ?>
-			</a>
+			</button>
+		</label>
+	</div>
+	<div class="sv_setting <?php echo $this->get_module( 'sv_settings' )->get_prefix( 'reset' ); ?>">
+		<h4><?php _e( 'Reset to factory settings', 'sv100' ); ?></h4>
+		<div class="description">
+			<?php _e( 'All your settings will be removed and replaced with the default settings.', 'sv100' ); ?>
+		</div>
+		<label for="<?php echo $this->get_module( 'sv_settings' )->get_prefix( 'reset' ); ?>">
+			<button
+					class="button"
+					data-sv_admin_modal='[{"title":"<?php echo $reset_modal['title']; ?>","desc":"<?php echo $reset_modal['desc']; ?>","type":"<?php echo $reset_modal['type']; ?>"}]'
+					data-sv_admin_ajax='[{"action":"<?php echo $this->get_module( 'sv_settings' )->get_prefix( 'reset' ); ?>"}]'
+			>
+				<?php _e( 'Reset Settings', 'sv100' ); ?>
+			</button>
 		</label>
 	</div>
 </div>
--- sv_settings.php
+++ sv_settings.php
@@ -13,42 +13,37 @@
 	
 	class sv_settings extends init {
 		public function init() {
-			// Module Info
-			$this->set_module_title( 'SV Settings' );
-			$this->set_module_desc( __( 'Import and export your settings.', 'sv100' ) );
-	
-			// Section Info
-			$this->set_section_title( __( 'Settings Import/Export', 'sv100' ) );
-			$this->set_section_desc( __( 'Import and export your settings', 'sv100' ) );
-			$this->set_section_type( 'tools' );
-			$this->get_root()->add_section( $this )
+			$this->set_module_title( 'SV Settings' )
+				 ->set_module_desc( __( 'Import and export your settings.', 'sv100' ) )
+				 ->check_first_load()
+				 ->register_scripts()
+				 ->set_section_title( __( 'Settings Import/Export', 'sv100' ) )
+				 ->set_section_desc( __( 'Import and export your settings', 'sv100' ) )
+				 ->set_section_type( 'tools' )
+				 ->get_root()
+				 ->add_section( $this )
 				 ->set_section_template_path( $this->get_path( 'lib/backend/tpl/tools.php' ) );
-	
-			// Loads Settings
-			$this->check_first_load()->register_scripts();
 			
 			// Action Hooks
 			add_action( 'wp_ajax_' . $this->get_prefix( 'export' ) , array( $this, 'settings_export' ) );
+			add_action( 'wp_ajax_' . $this->get_prefix( 'reset' ), array( $this, 'settings_reset' ) );
 		}
 		
 		protected function check_first_load(): sv_settings {
 			if ( $this->is_first_load() ) {
 				$this->settings_import( file_get_contents( $this->get_path( 'lib/backend/settings/default.json' ) ) );
 			}
-	
+			
 			return $this;
 		}
 		
 		protected function register_scripts(): sv_settings {
 			// Register Styles
-			$this->scripts_queue['tools'] =
-				static::$scripts
-					->create( $this )
-					->set_ID( 'tools' )
-					->set_path( 'lib/backend/css/tools.css' )
-					->set_inline( true )
-					->set_is_backend()
-					->set_is_enqueued();
+			$this->get_script( 'tools' )
+				 ->set_path( 'lib/backend/css/tools.css' )
+				 ->set_inline( true )
+				 ->set_is_backend()
+				 ->set_is_enqueued();
 			
 			return $this;
 		}
@@ -61,7 +56,7 @@
 			foreach ( $settings as $name => $value ) {
 				$settings[ $name ] = array_filter( $value );
 			}
-			
+
 			header( 'Content-Type: application/json');
 			header( 'Content-Disposition: attachment;filename="' . $filename . '"' );
 			
@@ -70,18 +65,22 @@
 			wp_die();
 		}
 		
+		public function settings_reset() {
+			$this->settings_import( file_get_contents( $this->get_path( 'lib/backend/settings/default.json' ) ) );
+			
+			echo json_encode( array(
+				'notice'	=> true,
+				'msg' 		=> __( 'Successfully reseted all settings.', 'sv100' ),
+				'type'		=> 'success',
+			));
+			
+			wp_die();
+		}
+		
 		protected function settings_import( string $json_data ) {
 			$data = json_decode( $json_data, true );
 			
-			// Deletes all options that starts with "sv100_sv_" or "sv100_scripts_settings_"
-			// @todo: check if this can be removed
-			foreach ( wp_load_alloptions() as $option => $value ) {
-				if ( strpos( $option, 'sv100_sv_' ) === 0 || strpos( $option, '0_settings_sv100' ) === 0 ) {
-					delete_option( $option );
-				}
-			}
-			
-			// @todo: implement nonce-check
+			$this->delete_options();
 			
 			// Sets all new options
 			foreach ( $data as $name => $settings ) {
@@ -93,8 +92,16 @@
 					if ( $name !== 'sv100_scripts_settings' ) {
 						$option = $name . '_settings_' . $setting;
 					}
-
+					
 					update_option( $option, $value, true );
+				}
+			}
+		}
+		
+		private function delete_options() {
+			foreach ( wp_load_alloptions() as $option => $value ) {
+				if ( strpos( $option, 'sv100_sv_' ) === 0 || strpos( $option, '0_settings_sv100' ) === 0 ) {
+					delete_option( $option );
 				}
 			}
 		}
